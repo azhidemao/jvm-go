83439c59438fe5747abcc76aae89a9c36957f72a
jiazhipeng <jiazhipeng@insdo.cn>
2020-02-20 16:39:45 +0800

    修改主程序入口

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import "fmt"
+import "strings"
+import "jvmgo/ch02/classpath"
 
 func main() {
 	cmd := parseCmd()
@@ -11,4 +13,18 @@ func main() {
 	} else {
 		startJVM(cmd)
 	}
+}
+
+func startJVM(cmd *Cmd) {
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classjre:%v classpath:%v class:%v args:%v\n",
+		cmd.XjreOption, cp, cmd.class, cmd.args)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	fmt.Printf("className:%v\n", className)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
 }
