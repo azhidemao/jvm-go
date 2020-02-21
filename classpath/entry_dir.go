7c3dd9c73c7b2cfea5ec832296aeb8c5e256916f
azhidemao <15718881030@163.com>
2020-02-21 10:08:10 +0800

    修改文件结构

--- classpath/entry_dir.go
+++ classpath/entry_dir.go
@@ -19,7 +19,6 @@ func (self *DirEntry) readClass(className string) ([]byte, Entry, error) {
 	fileName := filepath.Join(self.absDir, className)
 	data, err := ioutil.ReadFile(fileName)
 	return data, self, err
-
 }
 
 func (self *DirEntry) String() string {
