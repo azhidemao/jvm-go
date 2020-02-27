e2caa13bbaeacb072d09799bd6de43a38880593a
azhidemao <15718881030@163.com>
2020-02-27 10:24:31 +0800

    增加文件读取功能

--- classpath/entry_wildcard.go
+++ classpath/entry_wildcard.go
@@ -19,10 +19,12 @@ func newWildCardEntry(path string) CompositeEntry {
 		if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") {
 			jarEntry := newZipEntry(path)
 			compositeEntry = append(compositeEntry, jarEntry)
+
 		}
 
 		return nil
 	}
 	filepath.Walk(baseDir, walkFn)
+
 	return compositeEntry
 }
