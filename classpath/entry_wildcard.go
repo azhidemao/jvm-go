0b73712d81ae3c9718234d1e5f2f58bde28393f3
azhidemao <15718881030@163.com>
2020-02-27 11:00:17 +0800

    修复文件功能

--- classpath/entry_wildcard.go
+++ classpath/entry_wildcard.go
@@ -19,9 +19,7 @@ func newWildCardEntry(path string) CompositeEntry {
 		if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") {
 			jarEntry := newZipEntry(path)
 			compositeEntry = append(compositeEntry, jarEntry)
-
 		}
-
 		return nil
 	}
 	filepath.Walk(baseDir, walkFn)
